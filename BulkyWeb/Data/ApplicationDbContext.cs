8adeac2989277a3bd51bd39369f29b95d3dd5463
Abhishek Kumar Jha <abhishek.jha8387@gmail.com>
2024-03-26 16:04:55 +0530

    setting up DbContext

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            // if we have to do any registraion we will do in program.cs 
+
+
+        }
+
+
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // it will be the primary key of our table  , but how will I know this is primary key , we will use data annotation 
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        
+
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
