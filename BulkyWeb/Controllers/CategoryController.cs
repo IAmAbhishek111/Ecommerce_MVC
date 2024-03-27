c18613b77d04ded575e2492cf01a1864459d4c4c
Abhishek Kumar Jha <abhishek.jha8387@gmail.com>
2024-03-27 13:19:30 +0530

    ℹ️ [Feature] Implemented CategoryController with DbContext Integration and Category Table Seeding
    🚀 Description:
    - Created the CategoryController to manage categories in the application.
    - Integrated the DbContext for database operations, ensuring seamless interaction with the database.
    - Seeded initial data into the Category table to populate it with essential categories.
    
    🌐 Changes:
    - Added CategoryController.cs for handling category-related functionality.
    - Integrated ApplicationDbContext for database connectivity.
    - Implemented seeding of the Category table with initial data.
    
    🎨 View Enhancements:
    - Designed and implemented view pages to display changes related to categories.
    - Ensured proper rendering and functionality for viewing category details on web pages.
    
    🔧 Fixes and Enhancements:
    - Addressed any potential issues related to database connectivity and category management.
    - Improved user experience through enhanced web page functionality.
    
    🔒 Security Considerations:
    - Ensured data integrity and security measures in database interactions.
    - Implemented necessary access controls and validations for category management.
    
    🔍 Review Checklist:
    - [ ] Code reviewed for best practices and efficiency.
    - [ ] Unit tests added for new functionality.
    - [ ] Documentation updated for any changes or new features.
    - [ ] Performance optimizations considered and implemented where applicable.
    
    📆 Date: 27/03/2024
    
    #github #commit #webdevelopment #database #mvc #coding

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // constructor called
+
+        public readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        /*This is the action method */
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList); // it will show category list  in UI
+        }
+    }
+}
