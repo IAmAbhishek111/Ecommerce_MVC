ef3c1f9c53144dbbda6231cf8bc8d140eff33ada
Abhishek Kumar Jha <abhishek.jha8387@gmail.com>
2024-03-28 11:33:24 +0530

    Added Edit and Delete Features in category section.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -48,5 +48,102 @@ namespace BulkyWeb.Controllers
 
         }
 
+        public IActionResult Edit(int? id )
+
+        {
+            if(id == null || id == 0) {
+                return NotFound();
+
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            /*Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit (Category obj)
+
+        {
+          /*  if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order and name cannot be exactly the same");
+
+            }
+*/
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);   // this line is telling us that we have to update the category object into category table
+
+
+
+                // to execute the changes 
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+
+        }
+
+
+        public IActionResult Delete(int? id)
+
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            /*Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id )
+
+        {
+            /*  if (obj.Name == obj.DisplayOrder.ToString())
+              {
+                  ModelState.AddModelError("name", "The display order and name cannot be exactly the same");
+
+              }
+
+            
+  */
+            Category obj = _db.Categories.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+           
+
+      
+
+
+        }
+
     }
 }
