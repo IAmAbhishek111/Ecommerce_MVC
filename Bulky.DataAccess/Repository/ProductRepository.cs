c1f5967d55b60f2aea7e8cb4a860f1725fad3932
Abhishek Kumar Jha <abhishek.jha8387@gmail.com>
2024-04-01 19:08:42 +0530

    Added Migrations for Product and Updated Database and Made Repository of the products

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.DataAcess.Data;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product> , IProductRepository
+    {
+       
+        
+
+            private ApplicationDbContext _db;
+
+            //That way, whatever DB context we get here, we will pass to the repository and the error goes away.
+            public ProductRepository(ApplicationDbContext db) : base(db)
+            {
+
+                _db = db;
+
+            }
+
+            public void update(Product obj)
+            {
+                _db.Products.Update(obj);
+            }
+
+       
+    }
+
+
+    }
+
